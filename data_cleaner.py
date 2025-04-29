08eba8abbac1455283357f852fdde32684575210
Marlon Kohn <126592834+mizaagi@users.noreply.github.com>
2025-04-29 15:33:37 +0000

    4/29/2025

--- data_cleaner.py
+++ data_cleaner.py
@@ -3,4 +3,5 @@ import pandas as pd
 def clean_data(filepath):
     df = pd.read_csv(filepath)
     df.columns = df.columns.str.strip()
-    df['Deaths/100 Recovered'] = df['Deaths/100 Recovered'].str.replace('inf', '0').astype(int)
+    df['Deaths/100 Recovered'] = df['Deaths/100 Recovered'].astype(str).str.replace('inf', '0').astype(float)
+    return df
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 import data_cleaner as dc
+
+df = dc.clean_data('./data/country_wise_latest.csv')
+print(df)
+sorted_df_confirmed = df.sort_values('Confirmed')
+sorted_df_confirmed.plot(x='Confirmed', y='Deaths', marker='o')
+plt.savefig('./plots/plot1')
