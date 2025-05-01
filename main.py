bb58b549a12c2d1c80ae14302072509c5c1355b1
Marlon Kohn <126592834+mizaagi@users.noreply.github.com>
2025-05-01 15:27:10 +0000

    fixed plot

--- main.py
+++ main.py
@@ -6,16 +6,15 @@ import data_cleaner as dc
 
 df = dc.clean_data('./data/country_wise_latest.csv')
 pd.options.display.float_format = "{:.10f}".format
-print(df.describe())
 sorted_df_confirmed = df.sort_values('Confirmed')
 
-
+plt.figure(figsize=(12,10))
 df_whovals = df.groupby('WHO Region')['Deaths/100 Cases'].mean()
 ax = sns.barplot(x=df_whovals.index, y=df_whovals.values, color='slateblue')
 ax.bar_label(ax.containers[0], fontsize=10)
 plt.title("Correlation of WHO Region and Deaths/100 Cases")
 plt.ylabel("Deaths/100 Cases")
-plt.xticks(rotation=90)
+plt.xticks(rotation=-20)
 plt.savefig('./plots/plot1')
 
 ax = sns.barplot(x=sorted_df_confirmed['Confirmed'], y=sorted_df_confirmed['Deaths/100 Cases'], color='slateblue')
